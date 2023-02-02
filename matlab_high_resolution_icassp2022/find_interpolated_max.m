98eb84c1b3480de0a3476b3395e3c53ec5595076
Peter Gerstoft <44826832+gerstoft@users.noreply.github.com>
2023-02-01 16:07:37 -0800

    Delete matlab_high_resolution_icassp2022 directory

--- matlab_high_resolution_icassp2022/find_interpolated_max.m
+++
@@ -1,6 +0,0 @@
-function doa = find_interpolated_max(phi_vec,b,index)
-doa = phi_vec(index); % doa for maximum sample
-if (index > 1) && (index < length(phi_vec)),
-    aux = index + [-1 0 1];
-    [~, doa] = parabel(phi_vec(aux),b(aux)); % doa for interpolated maximum
-end
